d613e9f8678c0e21b7f7c0ac240f50e0432b0e9e
mplacesalvarez <mplacesalvarez@danielcastelaor.org>
2021-02-19 10:50:56 +0100

    Initial commit

---
+++ src/com/codigomain/Main.java
@@ -0,0 +1,14 @@
+package com.codigomain;
+import libreriaIN_OUT.libreria;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Libreria obx= new Libreria();
+
+
+
+
+
+    }
+}
